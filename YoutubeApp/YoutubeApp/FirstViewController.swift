425e1de98d674f46716a920bb9576393bebe7aaf
ChristopherTonn <christopher.tonn@gmail.com>
2018-06-05 13:17:58 +0200

    001 • Generated the Project with two Views and a TabBar

---
+++ YoutubeApp/YoutubeApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  YoutubeApp
+//
+//  Created by Christopher on 05.06.18.
+//  Copyright © 2018 Christopher. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
